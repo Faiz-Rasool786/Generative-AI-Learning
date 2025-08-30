2ed2598957caf3cff3eb227ab59ebfaaf40c7c54
Faiz Rasool <faizrasul313@gmail.com>
2025-08-30 23:06:24 +0500

    Completed Day 1 Python basics with math, loops, and functions

--- 01_Python_Fundamentals/day1_start.py
+++ 01_Python_Fundamentals/day1_start.py
@@ -1,1 +1,19 @@
-print('Hello, Faiz! Welcome to your Generative AI journey ??') 
+# Day 1: Python Basics
+print('Hello, Faizi! Welcome to your Genrative AI Journey! 🚀')
+
+# --- Variables and Math ---
+x = 10
+y = 5
+print (f'Sum: {x + y}, Difference: {x - y}, Product: {x * y} , Quotient: {x / y}, Division: {x // y}, Modulus: {x % y}, Exponent: {x ** y}')
+
+# --- Loops Example ---
+for i in range(1, 6):
+    print(f'Loop iteration: {i}')
+    
+# --- Function Example ---
+def factorial(n):
+    results = 1
+    for i in range(1, n + 1):
+        results *= i
+    return results
+print(f'Factorial pf 5 is: {factorial(5)}')
